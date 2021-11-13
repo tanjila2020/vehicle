21437aa6a6e1b447075840e373df2bdec230f777
tanjila2020 <tanjila.tanji15@gmail.com>
2021-11-12 23:00:02 -0500

    geberated output csv

--- csv_handler.py
+++ csv_handler.py
@@ -1,27 +1,22 @@
 import pandas
-df = pandas.read_csv('vehicle dataset.csv',
-                    index_col='#',)
-# print(df[0:5])
+import math
 
+df = pandas.read_csv('vehicle dataset.csv',
+                     index_col='#')
 time_array = df['time'].unique()
 
 for time in time_array:
-    matched_loc = df.loc[df['time'] == time]
-    # print(matched_loc['name'])
-    # print(matched_loc['x'][0:5])
-    print(sum(matched_loc['x']), len(matched_loc))
-
-    df.loc[df['time'] == time,'centroid_x'] = sum(matched_loc['x']) / len(matched_loc)
-    df.loc[df['time'] == time,'centroid_y'] = sum(matched_loc['y']) / len(matched_loc)
+    matched_loc = df.loc[df['time'] == time]   #select the rows with the timestamp of sec
+    print('Time:', time, 'Matched rows:', len(matched_loc))
+    x1 = matched_loc['x']
+    y1 = matched_loc['y']
+    x2 = df.loc[df['time'] == time,'centroid_x'] = sum(matched_loc['x']) / len(matched_loc)
+    y2 = df.loc[df['time'] == time,'centroid_y'] = sum(matched_loc['y']) / len(matched_loc)
     
-    print(df[0:5])
-    # matched_loc['centroid_x'] = matched_loc['x'] / len(matched_loc)
-    # matched_loc['centroid_y'] = matched_loc['y'] / len(matched_loc)
+    df.loc[df['time'] == time,'distance'] = ((x2 - x1)**2 + (y2 - y1)**2).pow(1./2)
+    #math.sqrt( (x2 - x1)**2 + (y2 - y1)**2 )   
+    # math.hypot(x2 - x1, y2 - y1)
 
+    # print(df[0:5])
 
-    exit()
-
-print(df['time'].unique())
-print(len())
-
-# df[0:5].to_csv('output.csv')
+df.to_csv('output.csv')
