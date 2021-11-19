5564c371afe63cd2de95d22598ff041c61296817
tanjila2020 <tanjila.tanji15@gmail.com>
2021-11-18 23:51:18 -0500

    new logic and delete large file

--- csv_handler.py
+++ csv_handler.py
@@ -1,22 +1,22 @@
 import pandas
 import math
 
-df = pandas.read_csv('vehicle dataset.csv',
-                     index_col='#')
-time_array = df['time'].unique()
+def generate_output_file():
+    df = pandas.read_csv('dataset.csv', index_col='#')
+    time_array = df['time'].unique()
 
-for time in time_array:
-    matched_loc = df.loc[df['time'] == time]   #select the rows with the timestamp of sec
-    print('Time:', time, 'Matched rows:', len(matched_loc))
-    x1 = matched_loc['x']
-    y1 = matched_loc['y']
-    x2 = df.loc[df['time'] == time,'centroid_x'] = sum(matched_loc['x']) / len(matched_loc)
-    y2 = df.loc[df['time'] == time,'centroid_y'] = sum(matched_loc['y']) / len(matched_loc)
-    
-    df.loc[df['time'] == time,'distance'] = ((x2 - x1)**2 + (y2 - y1)**2).pow(1./2)
-    #math.sqrt( (x2 - x1)**2 + (y2 - y1)**2 )   
-    # math.hypot(x2 - x1, y2 - y1)
+    for time in time_array:
+        matched_loc = df.loc[df['time'] == time]   #select the rows with the timestamp of sec
+        print('Time:', time, 'Matched rows:', len(matched_loc))
+        x1 = matched_loc['x']
+        y1 = matched_loc['y']
+        x2 = df.loc[df['time'] == time,'centroid_x'] = sum(matched_loc['x']) / len(matched_loc)
+        y2 = df.loc[df['time'] == time,'centroid_y'] = sum(matched_loc['y']) / len(matched_loc)
+        
+        df.loc[df['time'] == time,'distance'] = ((x2 - x1)**2 + (y2 - y1)**2).pow(1./2) / 1000
 
-    # print(df[0:5])
 
-df.to_csv('output.csv')
+    df.to_csv('first_output.csv')
+
+
+generate_output_file()
--- ev.py
+++ ev.py
@@ -7,10 +7,10 @@ import random
 from IPython import embed
 import pandas as pd
 
+##parameter value
 no_of_servers = 3
-no_of_ap = 10
-no_of_vehicles = 5
-##parameter to calculate data size from sanaz paper
+no_of_ap = 5
+# parameter to calculate data size from sanaz paper
 # data_height = 200  # inpixel
 # data_width = 300  # inpixel
 # bit_depth = 30  # in bit
@@ -18,7 +18,8 @@ no_of_vehicles = 5
 
 # bandwidth = 1000  # in Mbps (megabit) this is when we consider equal share(simple model to calc transfer rate)
 no_of_ins = 1000  # in millions
-period = 40
+period = 100
+
 deadline = period
 
 # calculating local and edge capacity
@@ -29,58 +30,77 @@ no_of_cores = 1
 # millons of instructions per milisecond
 local_cpu_capacity = math.ceil(((v * (freq*1000) + o) * no_of_cores) * 0.001)
 #local_cpu_speed = math.floor(local_cpu_speed)
-local_execution_time = math.ceil(
-    no_of_ins/local_cpu_capacity)  # in millisecond
-edge_speed_factor = 7
-edge_execution_time = math.ceil(
-    local_execution_time/edge_speed_factor)  # in millisecond
+local_execution_time = math.ceil(no_of_ins/local_cpu_capacity)  # in millisecond
+edge_speed_factor = 3
+edge_execution_time = math.ceil(local_execution_time/edge_speed_factor)  # in millisecond
 
-#-------calculating transfer rate from paper model-------------#
-d= 2 # in kilometer
-p= random.randint(20,30) #Transmission power of each vehicle in(dBm)
-h= 127+30*(math.log(d,(2))) # channel gain, d is in km
-white_noise= 2*10**-13 #white gaussian noise in watt 
-data_size = np.random.uniform(0.4, 0.8) #in megabits
-bandwidth = 100 #in Mbps which is equal to 25MHz
-snr= p*h/white_noise
+# read vehicle data from csv
+df = pd.read_csv('first_output.csv', index_col='#')
+csv_length = len(df)
 
-print("SNR")
-print (snr)
-n_i = (math.log((1+snr),(2))) #uplink spectral efficiency
-transfer_rate = (bandwidth*no_of_ap *n_i)/no_of_vehicles
-transfer_rate = round(transfer_rate,2) 
-print("transfer_rate")
-print (transfer_rate)
+# new column made in csv
+df['transfer_time'] = None
+# df['avg_response_time'] = None
+# df['no_of_jobs_dropped'] = None
+
+## finding unique timestamp to get the total no of vehicles in that time 
+time_array = df['time'].unique()
+no_of_vehicles = len(df['name'].unique())
+temp_no_of_vehicles = 500
+# print('no_of_vehicles', no_of_vehicles)
 
 
+#for a particular time, calculating transfer_time for all vehicles
+for time in time_array:
+    if time == 29072:
+        # select the rows with the timestamp of sec
+        matched_loc = df.loc[df['time'] == time]
+        no_of_vehicle = matched_loc['name']
 
-# calculating transfer time
-#transfer_rate = (bandwidth*no_of_ap)/no_of_vehicles
-transfer_time = math.ceil((data_size/transfer_rate)*1000)  # in millisecond
+        for i, row in df.loc[df['time'] == time].iterrows():
+            d = row['distance']  # in kilometer
+            # Transmission power of each vehicle in(dBm)
+            p = random.randint(20, 30)
+            h = 127+30*(math.log(d, (2)))  # channel gain, d is in km
+            white_noise = 2*10**-13  # white gaussian noise in watt
+            # data_size = np.random.uniform(0.4, 0.8) #in megabits
+            data_size = 1.8  # in megabits
+            bandwidth = 100  # in Mbps which is equal to 25MHz
+            snr = p*h/white_noise
+            n_i = (math.log((1+snr), (2)))  # uplink spectral efficiency
+            transfer_rate = (bandwidth*no_of_ap * n_i) / temp_no_of_vehicles
+            transfer_rate = round(transfer_rate, 2)
+            #transfer_rate = (bandwidth*no_of_ap)/no_of_vehicles
+            transfer_time = math.ceil((data_size/transfer_rate)*1000)  # in millisecond
+            df.at[i, 'transfer_time'] = transfer_time
 
-print(edge_execution_time)
-print(local_execution_time)
-print(data_size)
-print(transfer_time)
-exit()
+print(df[1:5])
+# print(edge_execution_time)
+# print(local_execution_time)
+# print(data_size)
+# print(transfer_time)
 
+# making vehicle class to store its attributes
 vehicle = namedtuple(
     'vehicle', 'name no_of_ins data_size edge_exe_time transfer_time period deadline')
 vehicle_list = []
 
-for i in range(1, no_of_vehicles + 1):
+for i, row in df.loc[df['time'] == 29072][0:temp_no_of_vehicles].iterrows():
     v = vehicle(
-        name=f'v{i}',
+        name=row['name'],
         no_of_ins=no_of_ins,
         data_size=data_size,
         edge_exe_time=edge_execution_time,
-        transfer_time=transfer_time,
+        transfer_time=row['transfer_time'],
         period=period,
         deadline=deadline
     )
     vehicle_list.append(v)
 
+print('Total vehicle count' ,len(vehicle_list))
 
+
+#main code to do the scheduling of the vehicle jobs along with shufffling of the vehicle order
 def create_queue(vehicles, time_span):
     queue = []
     queued_vehicles = namedtuple(
@@ -116,7 +136,8 @@ def create_queue(vehicles, time_span):
     return queue
 
 
-span = sum([vehicle.period for vehicle in vehicle_list])
+span = 1000
+#span = sum([vehicle.period for vehicle in vehicle_list])
 queue = create_queue(vehicle_list, span)
 vehicle_period_map = {vehicle.name: vehicle.period for vehicle in vehicle_list}
 cpu_current_time = 0
@@ -131,8 +152,8 @@ ignored_jobs_df = pd.DataFrame({"name": [], "job": []})
 
 for vehicle in queue:
     if vehicle.name in discarded_job:
-        print(discarded_job)
-        print('ignored ', vehicle)
+        # print(discarded_job)
+        # print('ignored ', vehicle)
         discarded_job.remove(vehicle.name)
         ignored_jobs_df = ignored_jobs_df.append({
             'name': vehicle.name,
@@ -166,26 +187,26 @@ for vehicle in queue:
         period = (vehicle.job_no - 1) * vehicle_period_map[vehicle.name]
         #response_time = vehicle_end_time - period
         cpu_current_time = vehicle_end_time
-        print(job)
+        # print(job)
     else:
         period = (vehicle.job_no - 1) * vehicle_period_map[vehicle.name]
         #response_time = vehicle_end_time - period
         cpu_current_time = vehicle_end_time
         discarded_job.append(job.name)
-        print('---------', job)
+        # print('---------', job)
 
 # print(response_time_df)
 
 average_response_time = response_time_df[[
     'name', 'response_time']].groupby(['name']).mean()
 
-print(average_response_time)
+print('Average response time\n', average_response_time)
 # response_time_df.loc[(response_time_df['name'] == 'v5') & (response_time_df['job_no'] == 5.0)]
 
 # print(ignored_jobs_df)
 
 ignored_job_count = ignored_jobs_df.groupby(['name']).size()
 
-print(ignored_job_count)
+print('No of jobs dropped\n', ignored_job_count)
 
 # embed()
