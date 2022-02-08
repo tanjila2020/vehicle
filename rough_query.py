ba3901ded22ffcdd3c4da0a5b094ae85a2998c55
tanjila2020 <tanjila.tanji15@gmail.com>
2022-02-07 21:42:48 -0600

    new dataset

--- rough_query.py
+++ rough_query.py
@@ -7,13 +7,15 @@ print(last_row)
 # filtered = data.query('time >=28874 and time<= 29104+180')
 # vehicle_name_array = filtered['name'].unique()
 # print('total no of vehicles', len(vehicle_name_array))
-avg = 0
-for t in range(28874, 28893):
-    tb1 = data.query('time==@t')
-    tb2 = data.query('time==@t+180')
-    a = set(tb1['name'])
-    b = set(tb2['name'])
-    c = a-b
-    avg = avg+(len(c)/len(a))*100
-print (avg)
+######## to calc avg no of vehicles in that excel file#####
+# avg = 0
+# for t in range(28874, 28893):
+#     tb1 = data.query('time==@t')
+#     tb2 = data.query('time==@t+180')
+#     a = set(tb1['name'])
+#     b = set(tb2['name'])
+#     c = a-b
+#     avg = avg+(len(c)/len(a))*100
+# print (avg)
+
   
