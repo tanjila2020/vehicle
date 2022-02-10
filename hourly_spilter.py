f01ca78624d989bf7ecc0df071b286422793d2ef
tanjila2020 <tanjila.tanji15@gmail.com>
2022-02-10 01:05:20 -0600

    new dataset files

--- hourly_spilter.py
+++ hourly_spilter.py
@@ -1,10 +1,11 @@
 import pandas as pd
 from IPython import embed
 
-time_slots = [3600, 7200, 10800, 14400, 18000, 21600, 25200, 28800, 32400, 36000, 39600, 43200, 46800, 50400, 54000, 57600, 61200, 64800, 68400, 72000, 75600, 79200, 82800 ]
-
+#time_slots = [3600, 7200, 10800, 14400, 18000, 21600, 25200, 28800, 32400, 36000, 39600, 43200, 46800, 50400, 54000, 57600, 61200, 64800, 68400, 72000, 75600, 79200, 82800 ]
+time_slots = [3600, 10800, 86400]
 offset = 0
-no_of_rows =6000000
+#no_of_rows =6000000
+no_of_rows =60000000
 time = 0
 
 while time < 90000:
@@ -23,7 +24,7 @@ while time < 90000:
     print(filtered_times)
 
     for ft in filtered_times:
-        temp_df = df.query(f'time>{ft} and time<{ft+900} and x<14500 and x>12500 and y<14000 and y>12000')
+        temp_df = df.query(f'time>={ft} and time<={ft+900} and x<14500 and x>12500 and y<14000 and y>12000')
         temp_df.to_csv(f'{ft} - {len(temp_df)}.csv')
     
     
--- rough_query.py
+++ rough_query.py
@@ -1,8 +1,36 @@
 import pandas as pd
 from IPython import embed
-data = pd.read_csv("8am.csv")
-last_row = data.iloc[-1:]
-print(last_row)
+# data = pd.read_csv("5am.csv")
+# last_row = data.iloc[-1:]
+# print(last_row)
+
+# print("------")
+
+# df = pd.read_csv("11pm.csv")
+# last_row2 = df.iloc[-1:]
+# print(last_row2) 
+
+# time_array = df['time'].unique()
+# no_of_sec = len(time_array)
+# print("no of sec:", no_of_sec)
+
+
+offset = 0
+#no_of_rows =6000000
+no_of_rows =60000000000
+time = 0
+df = pd.read_csv("koln-pruned.tr",
+                    skiprows=offset, nrows=no_of_rows, header=None, delimiter=' ',
+                    names=['time', "name", 'x', 'y', 'speed'],
+                    dtype={"time": "int64", "name": "string", "x": "float64", "y": "float64"})
+offset += no_of_rows
+
+start_time = int(df['time'].head(1))
+end_time = int(df['time'].tail(1))
+
+print(start_time)  
+
+print(end_time)    
 
 # filtered = data.query('time >=28874 and time<= 29104+180')
 # vehicle_name_array = filtered['name'].unique()
