7063536c4a6cbc373ff4cfca0025eac1dedc3a14
tanjila2020 <70779060+tanjila2020@users.noreply.github.com>
2023-07-06 10:15:44 -0500

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,158 @@
+from datetime import time
+import math
+import numpy as np
+from collections import namedtuple
+from functools import reduce
+from random import shuffle
+import random
+from IPython import embed
+from numpy.lib.function_base import piecewise
+import pandas as pd
+from pandas import ExcelWriter
+from pandas import ExcelFile
+from pyrsistent import v
+from  csv_read import times, avg_no_of_vehicles, avg_speeds
+from scheduler import scheduling
+import time
+
+####declaring and initializing arrays
+#for test:
+# blind_d = [x for x in range(2, 17, 2)]
+blind_d = [1, 2, 4, 6, 8, 10, 12, 14, 16, 20]
+deadlines = np.zeros((len(blind_d), len(times)))
+c_ap = np.zeros((len(blind_d), len(times)))
+c_ser = np.zeros((len(blind_d), len(times)))
+res_times = np.zeros((len(blind_d), len(times)))
+transfer_times = np.zeros((len(blind_d), len(times))) 
+transfer_rates = np.zeros((len(blind_d), len(times))) 
+
+
+
+
+
+
+#calc deadlines
+for i in range(0,(len(blind_d))):
+    for j in range(0,(len(times))):
+        deadlines[i, j] = round(((blind_d[i]/avg_speeds[j])*1000))
+
+peak_vehicle_no = max(avg_no_of_vehicles)
+peak_time = avg_no_of_vehicles.index(peak_vehicle_no) #returns the index of the times array having max avg vehicle
+# peak_time=2
+###numeric input parameter
+data_size= 1.8 #megabits
+bandwidth = 1000 #in megabit per sec
+edge_execution_time = 16
+ap_inc = 2 # ap number increment to test
+server_inc = 5  # server number increment to test the server count
+start_time = time.time()
+
+for i in range(0,(len(blind_d))):
+    print("current blind_distance", i)
+
+    for j in range(0,(len(times))):
+
+        print("time:", j)
+        
+        transfer_time_max = (deadlines[i, j] - 2*edge_execution_time)
+        print("deadline", deadlines[i,j])
+        print("transfer time max:", transfer_time_max)
+        no_of_ap = math.ceil((data_size*1000*avg_no_of_vehicles[j])/(transfer_time_max*bandwidth))
+        # print("average number of vehicles", avg_no_of_vehicles[j])
+        no_of_ap = no_of_ap- ap_inc
+        deadline_missed = 100
+        offset =0
+        while (deadline_missed >0):
+            
+            no_of_ap += ap_inc
+            print("no of ap", no_of_ap)
+            transfer_rate = (bandwidth*no_of_ap)/(avg_no_of_vehicles[j])
+            # print("transfer rate", transfer_rate)
+            transfer_time = round(((data_size/transfer_rate)*1000))  # in millisecond
+            print("transfer time", transfer_time)
+            res_time_var = 100
+            no_of_server= 1- server_inc + offset
+            if no_of_server < 0:
+                no_of_server = 1
+            res_time = 0
+
+            print("no of ap after 1st while loop ", no_of_ap)
+            print("no of server before second while loop", no_of_server)
+
+            while (deadline_missed >0 and res_time_var >0.005):
+                no_of_server+= server_inc
+                deadline_missed, res_time_temp = scheduling(no_of_ap, no_of_server, transfer_time, edge_execution_time, avg_no_of_vehicles[j], deadlines[i,j])
+                res_time_var = abs((res_time- res_time_temp )/ res_time_temp)
+                res_time= res_time_temp
+                print("blind_distance", i)
+                print("time", j)
+                print("in 2nd while loop")
+                print("transfer time:", transfer_time)
+                print("no of servers after 2nd while loop,", no_of_server)
+            offset = no_of_server-4
+
+        
+        # storing configurations
+        c_ap[i,j] = no_of_ap
+        c_ser[i,j] = no_of_server
+        res_times[i,j] = res_time
+        transfer_times[i,j] = transfer_time
+        transfer_rates[i,j] = round((1.8/transfer_time), 2)  # transfer rate megabit per sec
+
+
+print("--- %s seconds ---" % (time.time() - start_time))
+# #finding peak and avg configuration
+peak_aps=[]
+peak_sers= []
+c_ap_avg= []
+c_ser_avg= []
+
+for i in range(0,(len(blind_d))):
+    p_ap = c_ap[i,peak_time]
+    peak_aps.append(p_ap)
+    p_ser = c_ser [i,peak_time]
+    peak_sers.append(p_ser)
+
+
+
+
+
+
+
+c_ap_avg = np.round(c_ap.mean(axis=1))
+c_ser_avg = np.round(c_ser.mean(axis=1))
+
+print("peak ap:", peak_aps)
+print("config server", c_ser)
+print("peak server:", peak_sers)
+print("average ap", c_ap_avg)
+print("average server", c_ser_avg)
+
+
+
+#writing all the results in csv files
+df1= pd.DataFrame(c_ap)
+df2= pd.DataFrame(c_ser)
+df3= pd.DataFrame(res_times)
+df4= pd.DataFrame(c_ap_avg)
+df5 = pd.DataFrame(c_ser_avg) 
+df6 = pd.DataFrame(peak_aps)
+df7= pd.DataFrame(peak_sers)
+df8 = pd.DataFrame(transfer_times)
+df9 = pd.DataFrame(transfer_rates)
+
+print("peak servers:",df7)
+
+df1.to_csv('config_ap.csv', index = False)
+df2.to_csv('config_server.csv', index = False)
+df3.to_csv('response_times.csv', index = False)
+df4.to_csv('avg_ap.csv', index = False)
+df5.to_csv('avg_server.csv', index = False)
+df6.to_csv('peak_ap.csv', index = False)
+df7.to_csv('peak_server.csv', index = False)
+df8.to_csv('transfer_times.csv', index = False)
+df9.to_csv('transfer_rates.csv', index = False)
+
+print("done")
+    
+    
---
+++ final.py
@@ -0,0 +1,145 @@
+from datetime import time
+import math
+import numpy as np
+from collections import namedtuple
+from functools import reduce
+from random import shuffle
+import random
+from IPython import embed
+from numpy.lib.function_base import piecewise
+import pandas as pd
+from pandas import ExcelWriter
+from pandas import ExcelFile
+from pyrsistent import v
+from  csv_read import times, avg_no_of_vehicles, avg_speeds
+from final_scheduler import final_scheduling
+
+####declaring and initializing arrays
+#for test:
+# blind_d = [x for x in range(2, 5, 2)]
+# blind_d = [x for x in range(2, 17, 2)]
+blind_d = [1, 2, 4, 6, 8, 10, 12, 14, 16, 20]
+
+deadlines = np.zeros((len(blind_d), len(times)))
+perc_deadline_miss = np.zeros((len(blind_d), len(times)))
+max_speed = np.zeros((len(blind_d), len(times))) 
+max_speed_avg = np.zeros((len(blind_d), len(times)))
+utilize_servers = np.zeros((len(blind_d), len(times))) 
+avg_res_times = np.zeros((len(blind_d), len(times)))
+max_res_times = np.zeros((len(blind_d), len(times)))
+
+#calc deadlines
+for i in range(0,(len(blind_d))):
+    for j in range(0,(len(times))):
+        deadlines[i, j] = round(((blind_d[i]/avg_speeds[j])*1000))
+
+
+c_ap_avg= np.genfromtxt('avg_ap.csv', delimiter=",")
+c_ser_avg= np.genfromtxt('avg_server.csv', delimiter=",")
+
+# print(c_ap_avg[0])
+
+
+c_ap= np.genfromtxt('config_ap.csv', delimiter=",")
+c_ser= np.genfromtxt('config_server.csv', delimiter=",")
+
+peak_aps = (c_ap.max(axis=1))
+peak_sers = (c_ser.max(axis=1))
+# print(peak_aps[4])
+# exit()
+
+
+
+
+
+
+
+   
+# exit()   
+
+
+
+
+
+
+data_size= 1.8 #megabits
+bandwidth = 1000 #in megabit per sec
+edge_execution_time = 16
+
+for i in range(0,(len(blind_d))):
+    for j in range(0,(len(times))):
+        print("blind distance: ", i)
+        print("time: ", j)
+        # no_of_ap = c_ap_avg[i]
+        no_of_ap = peak_aps[i]
+        print(" no of ap,", no_of_ap)
+        transfer_rate = (bandwidth*no_of_ap)/(avg_no_of_vehicles[j])
+        transfer_time = math.ceil((data_size/transfer_rate)*1000)  # in millisecond
+        print("transfer_time,", transfer_time)
+        # no_of_server = c_ser_avg[i]
+        no_of_server = peak_sers[i]
+        print("no of server,", no_of_server)
+        # exit()
+        # pers_d_miss, avg_res, max_res_time, server_util = final_scheduling(no_of_ap, no_of_server, transfer_time, edge_execution_time, avg_no_of_vehicles[j], deadlines[i,j])
+        pers_d_miss, avg_res, max_res_time= final_scheduling(no_of_ap, no_of_server, transfer_time, edge_execution_time, avg_no_of_vehicles[j], deadlines[i,j])
+
+        perc_deadline_miss[i,j] = pers_d_miss
+        avg_res_times[i,j] = avg_res
+        max_res_times[i,j] = max_res_time
+        # utilize_servers[i,j] = server_util
+        max_speed[i,j] = blind_d[i]/(max_res_time/1000) # in meter per second
+        max_speed[i,j] = round(max_speed[i,j], 3)
+
+        max_speed_avg[i,j] = blind_d[i]/(avg_res/1000) # in meter per second
+        max_speed_avg[i,j] = round(max_speed_avg[i,j], 3)
+
+
+print("scheduler running done")
+#writing all the results in csv files for avg config
+dff1= pd.DataFrame(perc_deadline_miss)
+dff2= pd.DataFrame(avg_res_times)
+# dff3= pd.DataFrame(utilize_servers)
+dff4= pd.DataFrame(max_speed)
+dff5= pd.DataFrame(max_res_times)
+# dff6= pd.DataFrame(max_speed_avg)
+
+
+
+
+# print(dff4)
+print("df making done")
+
+#####for avg config
+# dff1.to_csv('percentage_deadline_miss_avg.csv')
+# dff2.to_csv('avg_response_avg.csv')
+# dff4.to_csv('max_safe_speed_avg.csv')
+# dff5.to_csv('max_response_time_avg.csv')
+# dff6.to_csv('avg_max_safe_speed_avg.csv')
+
+##for peak config
+dff1.to_csv('percentage_deadline_miss_peak.csv')
+dff2.to_csv('avg_response_peak.csv')
+dff4.to_csv('max_safe_speed_peak.csv')
+dff5.to_csv('max_response_time_peak.csv')
+# dff6.to_csv('avg_max_safe_speed_peak.csv')
+
+
+
+# dff1.to_csv('percentage_deadline_miss_peak.csv')
+# dff2.to_csv('avg_response_peak.csv')
+# dff4.to_csv('max_safe_speed_peak.csv')
+# dff5.to_csv('max_response_time_peak.csv')
+
+
+# print("percentage of deadline_miss (peak):", perc_deadline_miss)
+# print("avg response time (peak):", avg_res_times)
+# print("server utilization(avg) new:", utilize_servers)
+# print("max_safe_speed(peak):", max_speed) 
+print("complete !!!!")  
+     
+
+
+
+
+
+
